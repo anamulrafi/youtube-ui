d1a629fc4ba7cdd8c5473be8b5d4e20fb1f1d9c9
AnamulRafi <anamulhoquerafi118399@gmail.com>
2022-09-25 23:45:21 +0600

    mat divider & mat icon are added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
-
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 import { ContentComponent } from './content/content.component';
-import { LicenseComponent } from './license/license.component';
-import { ToolbarComponent } from './toolbar/toolbar.component';
-import { UserProfileComponent } from './user-profile/user-profile.component';
 import { YoutubeHomePageComponent } from './youtube-home-page/youtube-home-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { ToolbarComponent } from './toolbar/toolbar.component';
+import { LicenseComponent } from './license/license.component';
+import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
+import { UserProfileComponent } from './user-profile/user-profile.component';
+
 
 @NgModule({
   declarations: [AppComponent,
@@ -22,7 +25,9 @@ import { YoutubeHomePageComponent } from './youtube-home-page/youtube-home-page.
     ToolbarComponent,
     UserProfileComponent,
     YoutubeHomePageComponent],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, BrowserAnimationsModule,
+    MatDividerModule, MatButtonModule, MatCardModule, MatIconModule
+  ],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
